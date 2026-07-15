e6caac07d330e23f36e134b0dce1459696f4713c
Bdasari <dasarip.minnu@gmail.com>
2026-07-15 20:36:17 +0530

    Create app.js

---
+++ 📄 server/src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Banking API"
+  });
+});
+
+export default app;
