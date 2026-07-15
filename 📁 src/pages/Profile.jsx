ecb83d35f00d1c75ec28429b2d0582893cf16838
Bdasari <dasarip.minnu@gmail.com>
2026-07-15 20:13:28 +0530

    Create Profile.jsx

---
+++ 📁 src/pages/Profile.jsx
@@ -0,0 +1,26 @@
+function Profile() {
+
+  return (
+    <div className="page">
+
+      <h2>User Profile</h2>
+
+      <hr />
+
+      <br />
+
+      <p><strong>Name :</strong> John Doe</p>
+
+      <p><strong>Email :</strong> john@example.com</p>
+
+      <p><strong>Account Number :</strong> 1234567890</p>
+
+      <p><strong>Branch :</strong> Bangalore</p>
+
+      <p><strong>Available Balance :</strong> ₹50,000</p>
+
+    </div>
+  );
+}
+
+export default Profile;
