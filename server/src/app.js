ff7b5a02977c608155301352fd7dfbced774ab8b
Bdasari <dasarip.minnu@gmail.com>
2026-07-15 20:49:45 +0530

    Delete server/src directory

--- server/src/app.js
+++
@@ -1,6 +0,0 @@
-import accountRoutes from "./routes/accountRoutes.js";
-import transactionRoutes from "./routes/transactionRoutes.js";
-
-app.use("/api/account", accountRoutes);
-
-app.use("/api/transactions", transactionRoutes);
