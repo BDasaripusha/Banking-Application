0b16069e00f16884dbaab4d4852893eeaa6b46aa
Bdasari <dasarip.minnu@gmail.com>
2026-07-15 19:47:20 +0530

    Create AuthContext.jsx

---
+++ 📁 src/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+function AuthProvider({children}){
+
+    const [user,setUser]=useState(null);
+
+    const login=(data)=>{
+        setUser(data);
+    };
+
+    const logout=()=>{
+        setUser(null);
+    };
+
+    return(
+
+        <AuthContext.Provider
+            value={{
+                user,
+                login,
+                logout
+            }}
+        >
+
+        {children}
+
+        </AuthContext.Provider>
+
+    );
+
+}
+
+export default AuthProvider;
